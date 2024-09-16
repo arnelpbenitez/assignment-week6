60f05711c11b5867953168062ef762f9bb1ecdbe
Arnel Benitez <benitez.arnel@gmail.com>
2024-09-16 14:55:57 +1000

    Answers to week 6 assignment

--- week6.client/src/components/stores/StoreTable.jsx
+++ week6.client/src/components/stores/StoreTable.jsx
@@ -1,84 +1,94 @@
-import { Component } from 'react';
-
+import { Component } from "react";
 
 export class StoreTable extends Component {
+	static displayName = StoreTable.name;
 
-    static displayName = StoreTable.name;
+	constructor(props) {
+		super(props);
+		this.state = { stores: [], loading: true };
+		this.addStore = this.addStore.bind(this);
+	}
 
-    constructor(props) {
-        super(props);
-        this.state = { stores: [], loading: true };
-        //this.addStores = this.addCustomer.bind(this);
-    }
+	componentDidMount() {
+		this.populateStoresData();
+	}
 
-    componentDidMount() {
-        this.populateStoresData();
-    }
+	static renderStoresTable(stores) {
+		return (
+			<table className="table table-striped" aria-labelledby="tableLabel">
+				<thead>
+					<tr>
+						<th>Id</th>
+						<th>Name</th>
+						<th></th>
+						<th></th>
+					</tr>
+				</thead>
+				<tbody>
+					{stores.map((store) => (
+						<tr key={store.id}>
+							<td>{store.id}</td>
+							<td>{store.name}</td>
+							<td>
+								<button>Update Store</button>
+							</td>
+							<td>
+								<button>Delete Store</button>
+							</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		);
+	}
 
+	render() {
+		let contents = this.state.loading ? (
+			<p>
+				<em>Loading...</em>
+			</p>
+		) : (
+			StoreTable.renderStoresTable(this.state.stores)
+		);
 
-    static renderStoresTable(stores) {
-        return (
-            <table className="table table-striped" aria-labelledby="tableLabel">
-                <thead>
-                    <tr>
-                        <th>Id</th>
-                        <th>Name</th>
-                        <th></th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {stores.map(store =>
-                        <tr key={store.id}>
-                            <td>{store.id}</td>
-                            <td>{store.name}</td>
-                            <td><button>Update Store</button></td>
-                            <td><button>Delete Store</button></td>
-                        </tr>
-                    )}
-                </tbody>
-            </table>
-        );
-    }
+		return (
+			<div>
+				<h1 id="tableLabel">Stores</h1>
+				<form onSubmit={this.addStore}>
+					<div>
+						<label>Name</label>
+						<input type="text" name="storename" />
+					</div>
+					<div>
+						<label>Location</label>
+						<input type="text" name="storelocation" />
+					</div>
+					<button type="submit">Add Store</button>
+				</form>
 
-    render() {
-        let contents = this.state.loading
-            ? <p><em>Loading...</em></p>
-            : StoreTable.renderStoresTable(this.state.stores);
+				{contents}
+			</div>
+		);
+	}
 
-        return (
-            <div>
-                <button onClick={this.addStores}>Add Stores</button>
-                <h1 id="tableLabel">Stores</h1>
-                {contents}
-            </div>
-        );
-    }
+	async addStore(e) {
+		e.preventDefault();
+		const data = await fetch("stores", {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({
+				id: 0,
+				name: e.target.elements.storename.value,
+				location: e.target.elements.storelocation.value,
+			}),
+		}).then((data) => data.json());
 
-    async addStores() {
+		this.setState({ stores: [...this.state.stores, data] });
+	}
 
-        
-
-        const data = await fetch(
-            'stores', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-                id: 0,
-                name: 'UK, London'
-            })
-        }).then((data) => data.json());
-
-        
-
-        this.populateStoresData();
-    }
-
-
-
-    async populateStoresData() {
-        const response = await fetch('stores');
-        const data = await response.json();
-        this.setState({ stores: data, loading: false });
-    }
+	async populateStoresData() {
+		const response = await fetch("stores");
+		const data = await response.json();
+		this.setState({ stores: data, loading: false });
+	}
 }
